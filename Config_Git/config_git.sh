0e84e36385b150688fb29ae489bdb0db34325b47
Prabhakar <prk327@gmail.com>
2018-06-15 21:55:25 +0530

    Update config_git.sh

--- Config_Git/config_git.sh
+++ Config_Git/config_git.sh
@@ -2,4 +2,4 @@
 clear
 echo "Good Morning!, World"
 git clone "https://github.com/prk327/Intro-to-git.git"
-python /home/ubuntu/workspace/Intro-to-git/Config_Git/intro.py
+prk327/bin/python /home/ubuntu/workspace/Intro-to-git/Config_Git/intro.py
